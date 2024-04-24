720a06e3d29eaebf736cdb023c3561a110ce25c2
Manoj Kumar Reddy Bhimavarapu <cb.en.u4eie17010@cb.students.amrita.edu>
2024-04-24 08:42:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring-Boot-Dev-Tools-and-Embedded-Servers
+ Spring Boot Demo Representation for configuring the Built-in Embedded Server and Developer Tools.
---
+++ src/main/java/com/springserver/demo/DemoProjectForSpringBootEmbeddedServersApplication.java
@@ -0,0 +1,13 @@
+package com.springserver.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringBootEmbeddedServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringBootEmbeddedServersApplication.class, args);
+	}
+
+}
